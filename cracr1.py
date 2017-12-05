acc92284ddf723e7151809bb09788aaea54b700d
DESKTOP-PU99G55\Valelab <jongmin.sung@gmail.com>
2017-12-05 11:15:50 -0800

    Update date loading and class structure

--- cracr1.py
+++ cracr1.py
@@ -6,14 +6,15 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import os
 
-class CRACR(object):
+
+class Data(object):
     def __init__(self):
         self.path = os.getcwd()
         self.path_split = self.path.split('\\')
         self.data_name = self.path_split[len(self.path_split)-1]
         
     def load(self):
-        self.sample_list = os.listdir(self.path)
+        self.sample_list = os.listdir(self.path) # list[sample1, sample2]
         self.samples = []
         
         for self.sample_name in self.sample_list:
@@ -21,22 +22,30 @@ class CRACR(object):
             self.cells = []
             
             for self.cell_name in self.cell_list:
-                file_to_load = self.path+'\\'+self.sample_name+'\\'+self.cell_name
-                img = np.array(Image.open(file_to_load))
+                Cell(self.cell_name)
+                file_name = self.path+'\\'+self.sample_name+'\\'+self.cell_name
+                
+                img = np.array(Image.open(file_name))
                 self.cells.append(img)
             self.samples.append(self.cells)
             
     def plot(self):
+        color = 'magma'
+        plt.close('all')
         for i in range(len(self.samples)):
             plt.figure(i)
-            for j in range(len(self.samples[i])):
-                plt.subplot(2, np.ceil(len(self.samples[i])/2), j+1)
-                plt.imshow(self.samples[i][j])
+            cell_num = len(self.samples[i])
+            for j in range(cell_num):
+                plt.subplot(np.floor(cell_num**0.5), np.ceil(cell_num/np.floor(cell_num**0.5)), j+1)
+                plt.imshow(self.samples[i][j], color)
         plt.show()
         
+class Cell(object):
+    def __init__(self):
+        pass
                 
 
 # Start  
-data = CRACR()
+data = Data()
 data.load()
 data.plot()
