adaa65e9a2f9781d45737e5ae5df5e5dcaf656fe
Jongmin Sung <jongmin.sung@gmail.com>
2017-12-05 09:39:14 -0800

    Data loading and plotting the individual cells are done.

--- cracr1.py
+++ cracr1.py
@@ -12,17 +12,31 @@ class CRACR(object):
         self.path_split = self.path.split('\\')
         self.data_name = self.path_split[len(self.path_split)-1]
         
-        self.dir_list = os.listdir(self.path)
-        self.dir_num = len(self.dir_list)
+    def load(self):
+        self.sample_list = os.listdir(self.path)
+        self.samples = []
         
-        for self.dir_name in self.dir_list:
-            self.file_list = os.listdir(self.path+'\\'+self.dir_name)
-            self.file_num = len(self.file_list)
+        for self.sample_name in self.sample_list:
+            self.cell_list = os.listdir(self.path+'\\'+self.sample_name)
+            self.cells = []
             
-            for self.file_name in self.file_list:
-                print(self.file_name)
-                self.img = Image.open(self.path+'\\'+self.dir_name+'\\'+self.file_name)
+            for self.cell_name in self.cell_list:
+                file_to_load = self.path+'\\'+self.sample_name+'\\'+self.cell_name
+                img = np.array(Image.open(file_to_load))
+                self.cells.append(img)
+            self.samples.append(self.cells)
+            
+    def plot(self):
+        for i in range(len(self.samples)):
+            plt.figure(i)
+            for j in range(len(self.samples[i])):
+                plt.subplot(2, np.ceil(len(self.samples[i])/2), j+1)
+                plt.imshow(self.samples[i][j])
+        plt.show()
+        
                 
 
 # Start  
 data = CRACR()
+data.load()
+data.plot()
