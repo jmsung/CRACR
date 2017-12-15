19281c13bd8532d160cb2acb1fe00bcbfcf22895
Jongmin Sung <jongmin.sung@gmail.com>
2017-12-14 21:01:13 -0800

    radial intensity plot is completed.

--- cracr1.py
+++ cracr1.py
@@ -8,7 +8,8 @@ class Sample()
 - sample_name, cell_list[], cell_num, cells = [Cell()]
 
 class Cell() 
-- cell_name, intensity[array], analysis(), 
+- cell_name, intensity[array], analysis(), bg(), rescale(), cm(), median_center()
+- radial_intensity()
 
 """
 
@@ -17,49 +18,58 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import os
-from scipy import ndimage
 
 color = 'magma'
 scale_factor = 100
+r_step = 5
 
 class Cell(object):
     def __init__(self, cell_name, sample_path):
         self.cell_name = cell_name
         self.cell_path = sample_path + '\\' + cell_name   
         self.intensity = np.array(Image.open(self.cell_path))
-
-    def bg(self):
-        self.intensity = self.intensity - self.intensity.min()
+        self.intensity = self.intensity - self.intensity.min() # BG subtraction
         
-    def rescale(self, scale):
-        self.intensity = self.intensity/scale
-
-    def cm(self):
+    def mc(self): # Median center
         I = self.intensity.copy()
-        cm_float = ndimage.measurements.center_of_mass(I)
-        cm_int = ([int(round(cm_float[0])), int(round(cm_float[1]))])
-        self.cm = cm_int
-        I[cm_int[0]-1:cm_int[0]+1,:] = I.max()
-        I[:,cm_int[1]-1:cm_int[1]+1] = I.max()
-        self.intensity_cm = I.copy()
-        
-    def median_center(self):
-        I = self.intensity.copy()
-        Ix = []
-        Iy = []
+        I = I/scale_factor
+        Ix = []; Iy = []
         for i in range(np.size(I, axis=0)):
             for j in range(np.size(I, axis=1)):
-                Ii = [i]*int(I[i,j])
-                Ij = [j]*int(I[i,j])
-                Ix += Ii
-                Iy += Ij
+                Ii = [i]*int(I[i,j]); Ix += Ii
+                Ij = [j]*int(I[i,j]); Iy += Ij
         Ix_mc = int(np.median(Ix))
         Iy_mc = int(np.median(Iy))
         self.mc= [Ix_mc, Iy_mc]
         I[Ix_mc-1:Ix_mc+1, :] = I.max()
         I[:, Iy_mc-1:Iy_mc+1] = I.max()
-        self.intensity_mc = I.copy()        
-
+        self.intensity_mc = I.copy()       
+        
+    def radial_intensity(self):
+        I = self.intensity.copy()
+        x_max = np.size(I, axis=0)
+        y_max = np.size(I, axis=1) 
+        x_mc = self.mc[0]
+        y_mc = self.mc[1]
+        r_max = np.max([x_mc, x_max-x_mc, y_mc, y_max-y_mc])
+        self.r = np.arange(r_step, r_max, r_step)
+        self.Ir = np.zeros(len(self.r))
+        self.Is = np.zeros(len(self.r))
+        for i in range(len(self.r)):
+            ix = []; iy = []
+            for x in range(x_max):
+                for y in range(y_max):
+                    dist = ((x-x_mc)**2.0 + (y-y_mc)**2.0)**0.5                 
+                    if dist < self.r[i]:
+                        if i == 0:
+                            ix.append(x); iy.append(y)
+                        else:
+                            if dist > self.r[i-1]:
+                                ix.append(x); iy.append(y) 
+            I_select = I[ix, iy]
+            self.Ir[i] = I_select.mean()
+            self.Is[i] = I_select.std()
+       
      
 class Sample(object):
     def __init__(self, sample_name, data_path):
@@ -90,12 +100,14 @@ class Data(object):
             cell_num = sample.cell_num
             for j in range(cell_num):
                 cell = sample.cells[j]
-                cell.bg()
-                cell.rescale(scale_factor)
-                cell.median_center()           
+                cell.mc()    
+                cell.radial_intensity()
+                print(j)
+                       
                 
     def plot(self):
         plt.close('all')
+        # Plot overall cell images
         for i in range(self.sample_num):
             plt.figure(self.sample_list[i])
             sample = self.samples[i]
@@ -108,6 +120,23 @@ class Data(object):
                 plt.imshow(cell.intensity_mc, color)
                 plt.title(cell.cell_name[-8:-4])
             plt.subplots_adjust(wspace=0.3, hspace=0.3)
+            
+        # Plot analysis of individual cells
+        for i in range(self.sample_num):
+            sample = self.samples[i]
+            cell_num = sample.cell_num
+            for j in range(cell_num):
+                cell = sample.cells[j]
+                plt.figure(cell.cell_name)
+                plt.subplot(231); plt.imshow(cell.intensity, color); plt.colorbar()  
+                plt.subplot(232); plt.imshow(cell.intensity_mc, color) 
+                plt.subplot(234); plt.errorbar(cell.r, cell.Ir, yerr=cell.Is, 
+                                    fmt='o', ecolor='g'); plt.axis([0, 150, 0, 25000])
+                plt.subplot(233); plt.hist([cell.intensity], bins='scott', normed=False, 
+                color='k', histtype='step', linewidth=2); plt.gca().set_yscale("log")
+                         
+            
+            
         plt.show()
                       
                           
