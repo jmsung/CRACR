41f7a0f10a3decdd4653178d9b6b9072e8bbcbf6
Jongmin Sung <jongmin.sung@gmail.com>
2018-03-26 10:39:33 -0700

    1) Centrofome finding is done based on peak finding algorithm. 2) Finding centrosome Z is based on spline interpolation.

--- centrosome_height.py
+++ centrosome_height.py
@@ -13,34 +13,80 @@ Data
 
 from __future__ import division, print_function, absolute_import
 import numpy as np
-from PIL import Image
+from scipy.ndimage.filters import gaussian_filter
 import matplotlib.pyplot as plt
-from matplotlib import cm
+from PIL import Image
 import os
 from skimage.feature import peak_local_max
-colors = ['Blues', 'Greens', 'Reds']
+from skimage import filters
+from scipy import interpolate
 
-def running_avg(x, n):
-    return np.convolve(x, np.ones((n,))/n, mode='valid')
+n_row = 512
+n_col = 512
+n_ch = 3
+channel = ['Nucleus', 'CRACR', 'Centrosome']
+color = ['Blues', 'Greens', 'Reds']
+nucleus_size = 40
+centrosome_size = 10
 
+
+class Centrosome(object):
+    def __init__(self, I, row, col):
+        self.row = row
+        self.col = col
+        size = centrosome_size
+        Iz = np.mean(np.mean(I[:,row-size:row+size,col-size:col+size], axis=2), axis=1)
+        self.Iz = Iz - np.min(Iz)
+#        self.Iz = self.Iz / np.max(self.Iz)
+       
 class Movie(object):
     def __init__(self, movie_name, movie_path):
-        self.movie_name = movie_name           
+        self.movie_name = movie_name    
+        self.movie_path = movie_path      
         file_list = os.listdir(movie_path) 
-
+        ch_list = []
         for i in range(len(file_list)):
-            if file_list[i][-3:] == 'tif':
-                self.file_path = movie_path + '\\' + file_list[i]              
-                self.movie = Image.open(self.file_path)
+            if file_list[i][0:1] == 'C':
+                ch_list.append(file_list[i])
+
+        movie = Image.open(movie_path+'\\'+ch_list[0])
+        self.n_frame = movie.n_frames
+        self.I = np.zeros((n_ch, self.n_frame, n_row, n_col), dtype=int)
+
+        for i in range(len(ch_list)):
+            movie = Image.open(movie_path+'\\'+ch_list[i])
+            ch = int(file_list[i][1:2])            
+            for j in range(self.n_frame): 
+                movie.seek(j) # Move to i-th frame   
+                self.I[ch-1, j,] = np.array(movie, dtype=int)
         
-        self.n_frame = self.movie.n_frames       
-        self.n_row = self.movie.size[1]
-        self.n_col = self.movie.size[0]
-          
-        self.I = np.zeros((self.n_frame, self.n_row, self.n_col), dtype=int)
-        for i in range(self.n_frame): 
-            self.movie.seek(i) # Move to i-th frame
-            self.I[i,] = np.array(self.movie, dtype=int)
+    def projection(self):
+        self.I_max = []
+        s = [5, 1, 1]
+        for i in range(n_ch):
+            I = self.I[i]
+            I_min = np.min(I, axis=0)
+            for j in range(self.n_frame):
+                I[j,] = I[j,] - I_min    
+            I_max = np.max(I, axis=0)
+            I_max = gaussian_filter(I_max, sigma=s[i])
+            I_max[I_max < np.max(I_max)/10] = 0
+            self.I_max.append(I_max)     
+                        
+    # Find local maxima from movie.I_max                    
+    def find_centrosomes(self): 
+        I = self.I_max[2]
+        I = gaussian_filter(I, sigma=1)
+        self.peak = peak_local_max(I, min_distance=centrosome_size)
+        self.c_row = self.peak[::-1,0]
+        self.c_col = self.peak[::-1,1]
+        self.n_centrosomes = len(self.peak[:, 1])
+        print(self.n_centrosomes, 'centrosomes')
+        self.centrosomes = []
+        for i in range(self.n_centrosomes):
+            centrosome = Centrosome(self.I[2], self.c_row[i], self.c_col[i])
+            self.centrosomes.append(centrosome)
+
 
 class Data(object):
     def __init__(self):
@@ -50,18 +96,73 @@ class Data(object):
         self.movie_list = os.listdir(self.data_path) 
         self.movie_num = len(self.movie_list)
         self.movies = [] 
-        for movie_name in self.movie_list:            
-            movie = Movie(movie_name, self.data_path + '\\' + movie_name)
+        for movie_name in self.movie_list: 
+            movie_path = self.data_path + '\\' + movie_name           
+            movie = Movie(movie_name, movie_path)
             self.movies.append(movie)
     
     def analysis(self):
-        pass
-        
+        for i in range(self.movie_num):
+            movie = self.movies[i]
+            movie.projection()
+            movie.find_centrosomes()
+
     def plot(self):
-        pass
+        plt.close('all')
+        # Plot overall movie images
+        for i in range(self.movie_num):
+            movie = self.movies[i] 
+            print(i)
+
+            # Figure 1
+#            fig1 = plt.figure(self.movie_list[i], figsize = (20, 15), dpi=300)    
+            fig1 = plt.figure(self.movie_list[i]) 
+            
+            sp1 = fig1.add_subplot(131)       
+            sp1.imshow(movie.I_max[0], cmap='viridis')
+            sp1.set_title('Nucleus')
+            
+            sp2 = fig1.add_subplot(132)     
+            sp2.imshow(movie.I_max[2], cmap='plasma')
+            sp2.scatter(movie.c_col, movie.c_row, s=150, facecolors='none', edgecolors='y')
+            sp2.set_title('Centrosome')            
+            
+            sp3 = fig1.add_subplot(133)     
+            sp3.imshow(movie.I_max[0], cmap='viridis', alpha=1)            
+            sp3.imshow(movie.I_max[2], cmap='plasma', alpha=0.5)
+            sp3.scatter(movie.c_col, movie.c_row, s=150, facecolors='none', edgecolors='y')
+            title = 'Centrosomes = %d' % (movie.n_centrosomes)          
+            plt.title(title)       
+            
+            # Figure 2
+            fig2 = plt.figure(self.movie_list[i]+'_centrosome')
+
+            Iz_max = []            
+            for j in range(movie.n_centrosomes):
+#            for j in range(1):
+                y = movie.centrosomes[j].Iz
+                x = np.arange(0, len(y), 1)
+                tck = interpolate.splrep(x, y, s=0)
+                xnew = np.arange(0, len(y), 0.01)
+                ynew = interpolate.splev(xnew, tck, der=0)
+                Iz_max.append(xnew[np.argmax(ynew)])
+                sp1 = fig2.add_subplot(4, 11,j+1)
+                sp1.plot(x, y, 'x', xnew, ynew)
+                
+#            sp2 = fig2.add_subplot(122)
+#            sp2.plot(Iz_max, 'ko', [np.median(Iz_max)]*movie.n_centrosomes, 'r:')
+                                                                   
+        plt.show()      
+
+#Covert lab: DeepCell
+#Cell segmentation
+#    https://github.com/luispedro/python-image-tutorial/blob/master/Segmenting%20cell%20images%20(fluorescent%20microscopy).ipynb
+#Jeremy's machine learning
+
               
 # Start  
 plt.close('all')
 data = Data()
 data.analysis()
 data.plot()
+
